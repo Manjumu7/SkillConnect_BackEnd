dca02958eb3b400959176c25f9b86543f47a5543
Manjunath Madar <manjunathmu7@gmail.com>
2026-01-14 21:06:02 -0800

    fixed user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        enum: ["firstName","lastName"],
-        required:true
+    fullName: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     },
-    role:{
-      role:String,
-      enum: ["user","mentor","company","admin"]
+    role: {
+        role: String,
+        enum: ["user", "mentor", "company", "admin"],
+        default:"user"
     },
-},{timestamps:true})
+}, { timestamps: true })
 
-export const User=mongoose.model("User",userSchema)
+export const User = mongoose.model("User", userSchema)

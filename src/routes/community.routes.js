c266df0c879fe183f7e69014eee9e1a99e03f5ea
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-01-26 22:53:25 +0530

    feat(routes): add community CRUD routes with RBAC

--- src/routes/community.routes.js
+++ src/routes/community.routes.js
@@ -1,8 +1,14 @@
-import express from "express"
-import { createCommunity } from "../controllers/community.controllers.js"
-import verifyToken from "../middlewares/auth.middleware.js"
-const router=express.Router()
+import express from "express";
+import { createCommunity, deleteCommunity, getAllCommunities, getCommunityById, updateCommunity } from "../controllers/community.controllers.js";
+import { authorizeRoles } from "../middlewares/authRole.middleware.js";
+import verifyToken from "../middlewares/auth.middleware.js";
 
-router.post("/create",verifyToken ,createCommunity)
+const router = express.Router();
 
-export default router
+router.post("/", verifyToken, authorizeRoles("admin"), createCommunity);
+router.get("/", getAllCommunities);
+router.get("/:id", getCommunityById);
+router.put("/:id", verifyToken, authorizeRoles("admin"), updateCommunity);
+router.delete("/:id", verifyToken, authorizeRoles("admin"), deleteCommunity);
+
+export default router;

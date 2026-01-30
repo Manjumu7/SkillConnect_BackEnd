870f97b83250a47ae65681ce3d13ff07944145f1
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-01-30 22:30:29 +0530

    feat(cloudinary & multer): add file uploads using multer and cloudinary

--- src/routes/community.routes.js
+++ src/routes/community.routes.js
@@ -2,13 +2,14 @@ import express from "express";
 import { createCommunity, deleteCommunity, getAllCommunities, getCommunityById, updateCommunity } from "../controllers/community.controllers.js";
 import { authorizeRoles } from "../middlewares/authRole.middleware.js";
 import verifyToken from "../middlewares/auth.middleware.js";
+import { upload } from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
-router.post("/", verifyToken, authorizeRoles("admin"), createCommunity);
+router.post("/", verifyToken, authorizeRoles("admin"), upload.single("bannerImage"), createCommunity);
 router.get("/", getAllCommunities);
 router.get("/:id", getCommunityById);
-router.put("/:id", verifyToken, authorizeRoles("admin"), updateCommunity);
+router.put("/:id", verifyToken, authorizeRoles("admin"), upload.single("bannerImage"), updateCommunity);
 router.delete("/:id", verifyToken, authorizeRoles("admin"), deleteCommunity);
 
 export default router;

22e797716b984c4f7d1b621686cbd017274f4993
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-02-14 11:35:39 +0530

    feat(cors): add CORS support to the application

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 
-import express from "express";
+import express from "express"
 import dotenv from "dotenv";
+import cors from "cors";
 import authRouter from "./routes/auth.route.js";
 import communityRouter from "./routes/community.routes.js";
 import batchRouter from "./routes/batch.route.js";
@@ -12,8 +13,18 @@ import reelRouter from "./routes/reel.routes.js";
 import reelUserRouter from "./routes/reelUser.routes.js";
 
 const app = express();
-
 dotenv.config();
+
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+
+
 app.use(express.json());
 
 app.use("/auth", authRouter);
